b5e47f4d88cc7dba58603c04eed59b1cc424f5ea
orores27 <sabah.errifi@gmail.com>
2022-04-12 17:18:35 +0200

    entraînement Damirdine

---
+++ script.js
@@ -0,0 +1,86 @@
+//********************************* Exercice page 1 ********************************************************/
+/************************************exercice 1 ************************************************************/
+// let exercice;
+// // console.log(exercice);
+/************************************exercice 2 ************************************************************/
+
+// let exo = true;
+// // console.log(exo);
+/************************************exercice 3 ************************************************************/
+
+// // let number1 = 4;
+// // let number2 = 2;
+// // let result = number1 + number2;
+// // console.log(result);
+/************************************exercice 4 ************************************************************/
+
+// // let lastName = "Doe";
+// // let firstName = "John";
+// // let city = "New-York";
+// // console.log(lastName, firstName, city);
+// //window.alert("Nom:" + lastName + "\nPrénom:" + firstName + "\nVille" + city);
+/************************************exercice 5 ************************************************************/
+
+
+// // Prompt Dialog Box ===> pour une boite de dialogue
+// // let prompt = "Bonjour";
+
+// // function test()
+//     let result = prompt("Quel est votre Prénom ?");
+//         alert('Bonjour '+ result);
+// // test();
+
+//*********************************exercice page 2 **********************************************************/
+/************************************exercice 1 ************************************************************/
+
+
+// let number = 4;
+// // console.log(number) 
+// function compter() {
+//     return number*2-3;
+// }
+// compter();
+// alert(compter());
+
+//********************************exercice 2 ****************************************************************/
+
+// let chiffre = 14;
+// // console.log(chiffre)
+// function diviser() {
+//     return chiffre%3;
+// }
+// alert(diviser());
+
+//*********************************exercice 3 ***************************************************************/
+
+// let numero = 5;
+// // console.log(multiplier)
+// function multiplier() {
+//     return numero*2-3;
+// }
+// alert(multiplier());
+
+//*********************************exercice 4 ***************************************************************/
+
+// exercice associé avec à une page testUnit.js : 
+// étape 1 : on crée une fonction 'multiplier'
+// étape 2 : on crée un paramètre de la fonction ici 'compter'
+//le paramètre 'compter' sert d'ancre pour appeler la valeur que je lui attribue et je l'appelle sur un autre fichier. en lui donnant la valeur que je veux calculer.
+
+// function multiplier(compter) {
+//     console.log(compter*10);
+// }
+
+//*********************************exercice 4 ***************************************************************/
+
+//Création d'une 'fonction' qui permet de compter le nombre de lettre dans ma variable 'text'. On utilise la fonction = length//
+let text = 'Sabah est en galère.';
+let length = text.length;
+let para = document.getElementById('prenom').innerHTML = length;
+
+//*********************************exercice 5 ***************************************************************/
+
+
+
+
+
